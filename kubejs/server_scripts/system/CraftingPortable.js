0fd19b9518ba9b2d2008942b91ff82c8562948e8
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-12 15:50:58 +0900

    Update CraftingPortable.js
    Signed-off-by: Konnitiwa768 <konnitiwaa1@outlook.jp>

--- kubejs/server_scripts/system/CraftingPortable.js
+++ kubejs/server_scripts/system/CraftingPortable.js
@@ -1,4 +1,4 @@
-ItemEvents.rightClicked(event => {
+ItemEvents.LeftClicked(event => {
   if (event.item.id === 'minecraft:crafting_table') {
     event.player.openScreen('minecraft:crafting_table')
   }
